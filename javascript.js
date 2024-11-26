047c26fed2c36202302c0a570e8f1d675b5a6a18
paulBlartCallMop <188939308+paulBlartCallMop@users.noreply.github.com>
2024-11-26 16:44:52 -0500

    Add clear button

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ numkeys.forEach((key) => {
     numpad.appendChild(button);
 })
 
-const opKeys = ['+','-','×','÷','='];
+const opKeys = ['C','+','-','×','÷','='];
 const operators = document.querySelector('.operators')
 
 opKeys.forEach((key) => {
@@ -23,6 +23,19 @@ let num2 = '';
 let operation;
 const input = document.querySelector('.input');
 const display = document.querySelector('.display');
+function performOperation(a,b,op) {
+    switch(op) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '×':
+            return a * b;
+        case '÷':
+            if (b === 0) {alert('Don\'t divide by 0');}
+            return Math.floor(a * 100 / b)/100;   
+    }
+}
 
 input.addEventListener('click',(event)=>{
     let key = event.target.textContent;
@@ -34,9 +47,21 @@ input.addEventListener('click',(event)=>{
         }
     }
     if (opKeys.includes(key)) {
-        if (key === '=') {
-            alert('cool');
+        if (key === 'C') {
+            num1 = '';
+            num2 = '';
+            operation = undefined;
+        } else if (key === '=' || operation != undefined) {
+            if ((num1 === '')||(num2 === '')||(operation === undefined)) {
+                alert("Please make a valid formula.");
+                return;
+            }
+            let res = performOperation(parseFloat(num1),parseFloat(num2),operation);
+            num1 = res;
+            num2 = '';
+            operation = (key === '=') ? undefined:key;
         } else {
+            if (num1 == '') {num1 = '0';}
             operation = key;
         }
     }
